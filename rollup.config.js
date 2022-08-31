e194ef00d1293e3003fcb91903385f33ec6f944f
franxplore <104774353+franxplore@users.noreply.github.com>
2022-08-31 13:35:42 +0200

    first commit

---
+++ rollup.config.js
@@ -0,0 +1,36 @@
+import resolve from "@rollup/plugin-node-resolve";
+import image from "@rollup/plugin-image";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+
+const packageJson = require("./package.json");
+
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      image(),
+      typescript({ tsconfig: "./tsconfig.json"}),
+    ],
+  },
+  {
+    input: "dist/esm/index.d.ts", // had to be corrected: dist/esm/types/index.d.ts -> dist/esm/index.d.ts
+    output: [{file: "dist/index.d.ts", format: "esm"}],
+    plugins: [dts()],
+  }
+]
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+interface ButtonProps {
+    label: string;
+}
+
+const Button = (props: ButtonProps) => {
+  const {label} = props;  
+  return (
+    <button>{label}</button>
+  );
+}
+
+export default Button;
